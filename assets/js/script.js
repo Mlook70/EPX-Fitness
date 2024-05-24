4dc78ae458da7a082b8ac0e96dcef87ed5a1f914
Mlook70 <Abdulmalek.akel@gmail.com>
2024-05-24 06:45:13 +0300

    Update the UI/UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
-/**
- * Helper function to add event listeners to multiple elements
- */
+// Helper function to add event listeners to multiple elements
 function addEventOnElem(elems, event, callback) {
   elems.forEach(elem => elem.addEventListener(event, callback));
 }
 
-/**
- * Navbar toggle Javascript
- */
-
+// Navbar toggle Javascript
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 
-const toggleNavbar = function () { navbar.classList.toggle("active"); }
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+}
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
-const closeNavbar = function () { navbar.classList.remove("active"); }
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+}
 
 addEventOnElem(navLinks, "click", function() {
   // Remove 'active' class from all links
@@ -30,10 +29,7 @@ addEventOnElem(navLinks, "click", function() {
   closeNavbar();
 });
 
-/**
- * header & back top btn active
- */
-
+// Header & back to top button active state
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
